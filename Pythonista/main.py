500164ead7e6dfc9df26a87db77e099390f4d6aa
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2024-07-25 22:53:54 -0300

    Update main.py

--- Pythonista/main.py
+++ Pythonista/main.py
@@ -1,7 +1,13 @@
-objetos = ["Mouse","teclado","oculos"]
+vagas = [
+    ["vaga 1", 2900],
+    ["vaga 2", 1200],
+    ["vaga 3", 5000]
+]
 
-print(objetos)
-
-#
-
-print(list(range(132)))
+def compara_salario(vagas):
+    if vagas[1] >= 2500:
+        return True
+    else:
+        return False
+    
+print(list(filter(compara_salario, vagas)))
