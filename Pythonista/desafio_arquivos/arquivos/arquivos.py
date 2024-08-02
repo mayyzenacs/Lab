d68aed988bff0521c8367762361981834de0201a
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2024-08-02 07:54:55 -0300

    .

--- Pythonista/desafio_arquivos/arquivos/arquivos.py
+++ Pythonista/desafio_arquivos/arquivos/arquivos.py
@@ -1,12 +1,26 @@
 import os
 
+frutas = ["carambola","mexerica","maça","uva","acerola"]
+cores = ["roxo","amarelo","verde","rosa","azul"]
+linguagens = {"python","java","C#","Rust","PHP"}
 
-         
-if os.path.isdir("frutas.txt") and os.path.isdir("frutas.txt") and os.path.isdir("frutas.txt"):
-    print("diretorio ja ta criado")
-else:
-    os.mkdir("frutas.txt")
-    os.mkdir("cores.txt")
-    os.mkdir("linguagens.txt")
 
-with open("frutas.txt"):
+with open("frutas.txt", "a+", encoding="cp1252") as arquivo_frutas:
+    #arquivo_frutas.write(f"\n{cores}")
+    for item in frutas:
+        arquivo_frutas.write(f"\n{item}")
+    for cor in cores:
+        arquivo_frutas.write(f"\n{cor}")
+    arquivo_frutas.seek(0)
+    ler = arquivo_frutas.read()
+    print(ler)
+        
+    
+
+with open("linguagens.txt", "a") as arquivo_linguagens:
+    for item in linguagens:
+        arquivo_linguagens.write(f"\n{item}")
+
+# for i in range(5):
+#     os.mkdir("arquivo")
+
