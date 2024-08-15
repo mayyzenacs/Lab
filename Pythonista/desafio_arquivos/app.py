5bcbf7ff3761095b0348380ef6b8864ee6e31717
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2024-08-15 07:22:50 -0300

    Update app.py

--- Pythonista/desafio_arquivos/app.py
+++ Pythonista/desafio_arquivos/app.py
@@ -1,29 +1,11 @@
-import os
-
-# print(os.name) #listar OS
-# print(os.listdir) #listar arquivos no diretorio atual
-# print(os.getcwd) #retorna o path do diretorio atual
-# print(os.sep) #retorna o separador do sistema
-# print(os.path.join(os.getcwd() + os.sep + "nomearquivo.txt")) #reotnra path do arquivo
-# print(os.path.join(os.getcwd() + os.sep + "pasta" + "nomearquivo")) #reotnra um arquivo dentro de uma pasta 
-
-desafio_arquivos = os.path.join(os.getcwd()+ os.sep + "Pythonista" + os.sep + "desafio_arquivos")
-
-boleto = os.path.join(os.getcwd()+ os.sep + "Pythonista" + os.sep + "desafio_arquivos" + os.sep + "boleto.pdf")
-roteiro = os.path.join(os.getcwd()+ os.sep + "Pythonista" + os.sep + "desafio_arquivos" + os.sep + "roteiro.txt")
-planilha = os.path.join(os.getcwd()+ os.sep + "Pythonista" + os.sep + "desafio_arquivos" + os.sep + "planilha.xlsx")
-
-texto = os.path.join(os.getcwd()+ os.sep + "Pythonista" + os.sep + "desafio_arquivos" + os.sep + "desafio_arquivos_texto" + os.sep + "texto_1.txt")
-
-diretorio = "desafio_arquvios"
-print(os.listdir(desafio_arquivos))
-print(boleto)
-print(planilha)
-print(roteiro)
-
-print("="*20)
-
-print(texto)
-
-os.mkdir("musicas")
-os.makedirs("musicas"+ os.sep + "rock") 
+class metodo():
+    def metodo_instancia(self):
+        return('retornando instancia', self)  
+    
+    @classmethod
+    def metodo_classe(cls):
+        return('retornando classe', cls)
+    
+    @staticmethod
+    def metodo_estatico():
+        return("retornando estatico")
