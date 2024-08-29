3e962c164a04b4cc5c37c17049e1529363509615
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2024-08-28 21:49:50 -0300

    Update main.py

--- Pythonista/main.py
+++ Pythonista/main.py
@@ -1,20 +1,3 @@
-from abc import ABC, abstractmethod
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
-class monitor(ABC):
-    @abstractmethod
-    def aumentar_claridade(self, nivel):
-        pass
-
-    def diminuir_claridade(self, nivel):
-        pass
-
-
-class monitor_full_hd(monitor):
-    def aumentar_claridade(nivel):
-        print(f"aumentado o brilho em {nivel}")
-
-    def diminuir_claridade(nivel):
-        print(f"diminuindo o brilho em {nivel}")
-
-monitor_full_hd.aumentar_claridade(5)
-monitor_full_hd.diminuir_claridade(6)
+server = HTTPServer(("localhost", 8000), BaseHTTPRequestHandler)
