55d70ebd26ca33d9b9ea1ea355850deec3e8bf04
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2024-08-30 13:58:50 -0300

    Update main.py

--- Pythonista/main.py
+++ Pythonista/main.py
@@ -1,3 +1,23 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 server = HTTPServer(("localhost", 8000), BaseHTTPRequestHandler)
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/html; chrset=utf-8")
+        self.end_headers()
+        data = f""""
+        <html>
+            <head>
+                <title>Olá, mundo!</title>
+            </head>
+            <body>
+                <p>Testando servidor HTTP python</p>
+                <p>Diretorio: {self.path}</p>
+            </body>
+        """.encode()
+        self.wfile.write(data)
+        
+server = HTTPServer(("localhost",8000), SimpleHandler)
+server.serve_forever()
