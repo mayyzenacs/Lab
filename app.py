9995170b9a3945e8641fa0dcb383df4227871930
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2024-09-11 22:07:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,11 @@ class Clientes:
         self.endereço = endereço
         self.historico_compras = []
 
-    def realizar_compra(self, pedido):
+    def realizar_pedido(self, pedido):
         self.historico_compras.append(pedido)
+
+    def contar_pedidos(self):
+        return len(self.historico_compras)
 
     def __str__(self):
         compras_str = ",".join([str(pedido) for pedido in self.historico_compras])  
@@ -36,12 +39,10 @@ class Pedidos:
         self.cliente = cliente
         self.lista_produtos = lista_produtos
         self.data = data
-        cliente.realizar_compra(self)
+        cliente.realizar_pedido(self)
 
     def calcular_total(self):
-        for produto in self.lista_produtos:
-            total = sum(self.lista_produtos)
-        return total
+        return sum(produto.preço for produto in self.lista_produtos)
     
 
 produto1 = Produtos("parafusadeira", 250, 10, "parafusadeira 12v")
@@ -53,14 +54,19 @@ cliente1 = Clientes("Carlos", "Rua Benedito, 12")
 cliente2 = Clientes("Renata", "Rua Suslis, 234")
 cliente3 = Clientes("Leandro", "Av Joao Renan, 1234, Bloco B")
 
-
 pedido1 = Pedidos(cliente1,[produto1, produto3], "23-05-2024")
+pedido2 = Pedidos(cliente1, [produto2],"03-01-2023")
+pedido2 = Pedidos(cliente2,[produto1, produto2], "12-09-2024")
+pedido3 = Pedidos(cliente3, [produto3],"29-03-1999")
+
+print(f"Cliente: {cliente1.nome} numero de pedidos: {cliente1.contar_pedidos()}")
+print(f"Cliente: {cliente2.nome} numero de pedidos: {cliente2.contar_pedidos()}")
+print(f"Cliente: {cliente3.nome} numero de pedidos: {cliente3.contar_pedidos()}")
 
 
-print(str(cliente1))
-
-
-
+print(pedido1.calcular_total())
+print(pedido2.calcular_total())
+print(pedido3.calcular_total())
 
 
 
