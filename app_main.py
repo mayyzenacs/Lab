51c8516855b58a2d13ab1a1e33f4d3151004b405
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2024-09-20 22:31:44 -0300

    trabalho da facul

--- app_main.py
+++ app_main.py
@@ -1,4 +1,4 @@
-
+import matplotlib.pyplot as plt
 
 #definindo a classe principal e seus atributos
 class Livro():
@@ -7,30 +7,84 @@ class Livro():
         self.autor = autor
         self.gênero = gênero
         self.estoque = estoque
-        self.acervo = {} #definindo a lista vazia que receberá os livros que já existem
-        self.novoAcervo = [] #definindo a lista que recebe os novos livros
-    
-    #Função para cadastrar novos livros
-    def Cadastro(self, livro, autor, gênero, estoque):
-        livro_novo = Livro(livro, autor, gênero, estoque)
-        self.novoAcervo.append(livro_novo)
 
-    #Função para listar todos os livros
+    #Função para listar todos os livros que é chamada quando a opção de listar acervo é escolhida
     def __str__(self):
-        return f"Os livros são {self.titulo}"
-        
-    #Função para realizar a busca dos livros
-    def Busca(self, livro):
-        pass
-
+        return f"Livro: {self.titulo} Autor: {self.autor} Gênero: {self.gênero} Estoque: {self.estoque}"
+    
     #Função para gerar o gráfico com as informações 
     def Gerar_Gráfico(self, dados):
-        pass    
+        pass   
+
+acervo = []          #definindo a lista que recebe os novos livros
+
+#Função para realizar a busca dos livros
+def Busca(titulo):
+    for livro in acervo: #percorre a lista, lembrando que a lista tem cada instacia atribuida a classe 
+        if livro.titulo.lower() == titulo.lower(): #vai verificar utilizando a instancia dentro da lista
+            print()
+            print(f"--> livro {livro.titulo} ENCONTRADO! \n --> ESTOQUE: {livro.estoque}")
+            return livro
+        else:
+            print()
+            print("livro não encontrado")
+
+#Função para cadastrar novos livros
+def Cadastro(titulo, autor, gênero, estoque):
+    adicionar_livro = Livro(titulo, autor, gênero, estoque)
+    acervo.append(adicionar_livro)
+    print(f"Livro {titulo} do autor {autor} adicionado com sucesso")
+
+#Função para mostrar a lista completa de livros adicionados
+def listarAcervo():
+    for livro in acervo:
+        print("==========")
+        print(livro)
+
+print("=======================================================")
+print("===================BIBLIOTECA ONLINE===================")
+print("=======================================================")
+
+#loop que torna possivel o menu de opções e a possibilidade de selecionar outras opções
+while True:
+    print()
+    print("____________ MENU ____________")
+    print("""
+          1 - Adicionar livro\n 
+          2 - Listar acervo \n 
+          3 - Buscar Livro \n 
+          4 - Gerar Gráfico \n 
+          0 - SAIR
+          """)
+    entrada = int(input("Digite uma opção númerica: "))
+    
+    #esse if verifica qual opção foi digitada e resolve o resultado
+    if entrada == 1:
+        titulo = input("Digite TITULO: ").upper()
+        autor = input("Digite AUTOR: ").upper()
+        genero = input("Digite GÊNERO: ").upper()
+        estoque = int(input("Digite ESTOQUE: "))
+        Cadastro(titulo, autor, genero, estoque)
+        
+    elif entrada == 2:
+        print("ESTOQUE TOTAL")
+        print()
+        listarAcervo() #chama a função listar acervo la de cima para mostrar a lista
+    elif entrada == 3:
+        Busca(input("Digite o titulo do livro: ")) #recebe o nome do livro a buscar e manda para função de busca     
+    elif entrada == 4:
+      generos = []
+      for instancia in acervo:
+          generos(instancia.gênero)
+          cont = generos.count(instancia.gênero)
+      plt.bar(generos, cont, color="blue")
+      plt.xlabel("Gêneros")
+      plt.ylabel("Quantidade")
+      plt.title("Livros por gênero")
+    elif entrada == 0:
+        print()
+        print("Programa Encerrado!")
+        print("====================")
+        break
 
 
-livro1 = Livro("O Chamado de Cthulhu", "Lovecraft", "Terror Cósmico", 1)
-livro2 = Livro("Sussurros na Escuridão", "Lovecraft", "Terror Cósmico", 5)
-livro3 = Livro("Tudo que Destruimos", "Mayra", "Drama", 12)
-adicionar_Livro = livro1.Cadastro("O Pequeno Principe", "Antoine de Saint-Exupéry", "Fábula", 100)
-adicionar_Livro2 = livro1.Cadastro("A Culpa é das Estrelas", "John Green", "Romance", 50)
-print(livro1)
