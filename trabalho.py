a4096bdc5fb01199e8eb67af0ac0dd255be4434e
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2024-09-26 07:52:28 -0300

    .

--- trabalho.py
+++ trabalho.py
@@ -2,6 +2,8 @@
 import sqlite3
 import pandas as pd
 from time import sleep
+import matplotlib.pyplot as plt
+import seaborn as sb
 
 #criando a conexão com o banco de dados
 conexao = sqlite3.connect("dados_vendas.db")
@@ -42,24 +44,50 @@ INSERT INTO vendas1 (data_venda, produto, categoria, valor_venda) VALUES
 busca = "SELECT * FROM vendas1"
 cursor.execute(busca)
 
+#pega os dados para visualização
 resultado = cursor.fetchall()
+#utilizei o codigo comentado abaixo para visualizar a tabela
 #for row in resultado:
  #   print(row)
 
+#le os dados usando pandas, principalmente na funcionalidade de ler sql 
 df = pd.read_sql_query(busca, conexao)
 
-entrada = ""
+#função que faz a lógica para inserir novos dados no banco de dados
 def inserir_dados(cursor, data_venda, produto, categoria, valor_venda):
     cursor.execute("INSERT INTO vendas1 (data_venda, produto, categoria, valor_venda) VALUES (?, ?, ?, ?)", (data_venda, produto, categoria, valor_venda))
     conexao.commit()
 
+#calcula a media da coluna valor de venda utilizando funcionalidade do pandas
 def calcular_media():
     media = df["valor_venda"].mean()
     return f"A média dos valores de venda desses produtos é: ${media}"
 
-def porcentagem_coluna():
-    pass
+#cria a primeira tabela utilizando matplotlib
+def tabela():
+    cont = "SELECT categoria, COUNT(*) as vendas FROM vendas1 GROUP BY categoria"
+    df = pd.read_sql_query(cont,conexao)
+    plt.bar(df["categoria"], df["vendas"], color="blue")
+    plt.ylabel("Categoria")
+    plt.xlabel("Total Vendas")
+    plt.title("Vendas por Categoria")
+    return plt.show()
 
+#cria a segunda tabela utilizando seaborn
+def grafico():
+    dados = "SELECT categoria, valor_venda FROM vendas1"
+    df = pd.read_sql_query(dados,conexao)
+    plt.figure(figsize=(10,6))
+    sb.scatterplot(data=df, x='categoria', y='valor_venda')
+    plt.title('Distribuição dos Valores de Venda por Categoria')
+    plt.xlabel('Categoria')
+    plt.ylabel('Valor de Venda')
+    return plt.show()
+
+#defini entrada como nula para não ocorrer erro de que não existe
+entrada = ""
+
+#while para fazer o menu de opções
 while entrada != 0:
     print("__________MENU__________")
     print("""
@@ -70,30 +98,35 @@ while entrada != 0:
         (0) SAIR
     """)
     entrada = int(input("Digite uma opção -> "))
+    #try e finally para executar uma tarefa mesmo que a ultima nao tenha sido completada, e if e elif para verificar qual opção foi digitada
     try:
         if entrada == 1:
             print("Adicione novos produtos ao banco de dados")
-            data_venda = int(input("Data de venda: "))
+            data_venda = input("Data de venda: ")
             produto = input("Nome do produto: ")
             categoria = input("Categoria: ")
             valor_venda = float(input("Valor de carteira: "))
             inserir_dados(cursor, data_venda, produto, categoria, valor_venda)
+            print("Produto inserido com sucesso")
         elif entrada == 2:
             print(calcular_media())
         elif entrada == 3:
             print("Determinando situação...")
             sleep(5)
             print("Aqui está seu relátorio da situação")
-            print(f"No banco de dados há o total de {df["produto"].count()} produtos, sendo a média total do valor ${df['valor_venda'].mean()}")
-            
-
-            
+            contagem = df["produto"].count()
+            media_contagem = df['valor_venda'].mean()
+            print(f"No banco de dados há o total de {contagem:.2f} produtos, sendo a média total do valor ${media_contagem:.2f}")     
         elif entrada == 4:
-            pass
+            print("Gerando os gráficos...")
+            sleep(5)
+            tabela()
+            grafico()
     finally:
+        #commit em todas as mudanças 
         conexao.commit()
-        print("Programa encerrado")
 
-
-
+#fecha a conexão
 conexao.close()
+print("programa encerrado")
+
