4550b9bc14dcb21e78fca9e3b7989055e5d7fc8f
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2024-10-21 07:24:47 -0300

    Update app5.py

--- app5.py
+++ app5.py
@@ -1,16 +1,22 @@
 import threading
 
 site = 'site'
+dados = ['celular','monitor', 'fone de ouvido', 'alto falante', 'computador']
+
 
 def entrando_site(site):
     for i in range(5):
         print(f'entrando no site {i}')
 
 def extraindo_dados():
+    for item in dados:
+        print(item)
 
 
-dados = ['celular','monitor', 'fone de ouvido', 'alto falante', 'computador']
 
 threads = []
 for t in dados:
     new_thread = threading.Thread(target=entrando_site, args=(site,), daemon=True)
+new_thread.start()
+extraindo_dados()
+new_thread.join()
