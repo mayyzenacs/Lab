048aaf456e9f0c7db866d87839bbe1dce8b4f5d6
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2024-11-04 22:56:41 -0300

    ,

--- eventos-api/appy.py
+++ eventos-api/appy.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return '<h1>Flask configurado com sucesso</h1>'
 
+@app.route('api/eventos/')
+def listar_eventos():
+    return 'abc'
+
 
 if __name__ == "__name__":
     app.run()
