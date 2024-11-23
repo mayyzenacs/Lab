1667e8b9b09769252a57b5f04ee304f4bad00e5a
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2024-11-22 22:03:01 -0300

    Update settings.py

--- eventos-api/vamomarcar/settings.py
+++ eventos-api/vamomarcar/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# Build paths inside the project like this: BE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
