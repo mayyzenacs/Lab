c60c7410d96ae8b00912f53c88e9608c6f1d3232
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2024-12-24 07:53:02 -0300

    /

--- master/1.py
+++ master/1.py
@@ -1,26 +1,63 @@
 import json
 import os
+from datetime import date
 
 todos_list = "todos_list.json"
 
-def check():
+def load_checker():
     if not os.path.exists(todos_list): 
-          return "socorro"
+          return []
     else: 
         with open(todos_list, "r") as archive:
             return json.load(archive)
         
-def write(list):
-    with open(todos_list, "W") as archive:
-        json.dumps(list, archive)        
+def write(tasks):
+    with open(todos_list, "w") as a:
+        json.dump(tasks, a)        
 
-def new_task():
-    description = input("Enter a description to your task" )
-    status = input("Enter task status" )
+def new_task(tasks):
+    description = input("Enter a description to your task " )
+    status = input("Enter task status " )
+    task_id = len(tasks) + 1
+    createdAt = date.today()
+    updatedAt = date.today()
+    task = {
+        "id": task_id,
+        "description": description,
+        "status": status,
+        "createdAt": createdAt,
+        "updatedAt": updatedAt
+    }
+    tasks.append(task)
+    print(f"task ID{task_id} successfully created!")
 
 
 
-print(check())
+def main():
+    list = load_checker()
+    while True:
+        print("CLI Task Tracker")
+        print( """
+            1 - Add new task
+            2 - Update task
+            3 - Delete task
+            4 - View all tasks
+            5 - Exit
+            """)
 
 
+        option = int(input("Choose an menu option "))
+        if option == 1:
+            new_task(list)
+            write(list)
+        elif option == 2:
+            pass
+        elif option == 3:
+            pass
+        elif option == 4:
+            pass
+        elif option == 5:
+            break
+
+main()
 
