ed75f7abf0a7c8de06dc6b6b94cb527e8a59c1f8
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2025-05-02 22:55:04 -0300

    .

--- app3.py
+++ app3.py
@@ -1,20 +1,10 @@
+from datetime import time
 
-import threading
-
-site = 'site'
-
-def acessar_site(site):
-    for i in range(20):
-        print(f"acesssando site {i}/19")
-
-def baixando_fotos():
-    for i in range(20):
-        print(f"baixando fotos {i}/19")
+def anabanana(args):
+    return len(args)
 
 
-new_thread = threading.Thread(
-    target=acessar_site, args=(site,), daemon=True)
-new_thread.start()
-baixando_fotos()
-new_thread.join()
-#
+ana = anabanana(input("Digite um argumento"))
+
+print(ana)
+
