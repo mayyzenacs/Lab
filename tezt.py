9131e4d7e38eaad718a90d39cc20d1e8c085dbdf
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2025-07-20 10:49:03 -0300

    .

---
+++ tezt.py
@@ -0,0 +1,22 @@
+
+
+def calculo(a, b, operador): 
+    if operador == '+':
+        soma = a + b
+        return soma
+    elif operador == '-': 
+        sub = a - b 
+        return sub
+    elif operador == '/':
+        divi = a / b
+        return divi
+    elif operador == '*':
+        mult = a * b
+        return mult
+    
+resultado = calculo(5, 6, '*')
+print(resultado)
+
+def test():
+    assert calculo(5,6, '*') == 30
+
