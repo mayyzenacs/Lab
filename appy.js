1940e944bb80ecc02bfffca8fe6284d13eb147e5
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2025-08-27 20:37:21 -0300

    Update appy.js

--- appy.js
+++ appy.js
@@ -3,7 +3,13 @@ class Clientes{
         this.nome = nome, 
         this.idade = idade,
         this.estado = estado
+
     }
+    speak () {
+        console.log(`${this.nome} fala`)
+}
 }
 
-const cliente = New Clientes('lucas')
+const c = new Clientes('lucas', 10, 'ceara')
+
+c.speak()
