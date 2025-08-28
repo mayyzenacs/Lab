807642272d6bb848b3589623fb1a4b57ec94b166
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2025-08-27 23:06:53 -0300

    Update appy.js

--- appy.js
+++ appy.js
@@ -1,15 +1,6 @@
-class Clientes{
-    constructor(nome, idade, estado) {
-        this.nome = nome, 
-        this.idade = idade,
-        this.estado = estado
-
-    }
-    speak () {
-        console.log(`${this.nome} fala`)
+const pessoa = {
+    nome:'ana',
+    idade: 28
 }
-}
-
-const c = new Clientes('lucas', 10, 'ceara')
-
-c.speak()
+ 
+const {nome, idade} = pessoa
