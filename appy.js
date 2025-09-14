e4f432686539e2a4b4b4dd42740f985cdc704551
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2025-09-13 22:42:40 -0300

    .

--- appy.js
+++ appy.js
@@ -1,11 +1,35 @@
-function reversa (word) {
-    reverse = ''
-    for (let i = word.length - 1; i >= 0; i--) {
-        reverse += word[i]
-    }
-    if (reverse === word){
-        return 'sabonete'
+const readline = require('readline')
+
+const rl = readline.Interface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+function showMenu() {
+    console.log('\n===== MENU =====')
+    console.log('1 Calculadora comercial')
+    console.log('2 Ver calculo anterior')
+    console.log('0 Sair')
+    rl.question('\nEscolha uma opção', opcao => {
+        options(opcao)
+    })
+}
+
+function calculator(op, value) {
+    if (op === 1) {
+        let percent = 0.15
+        var originalValue = value / (percent - 1)
+        var finalValue = (originalValue * 100) / 100 
+        return finalValue
     }
 }
 
-reversa('ana')
+function options(op) {
+    switch (opcao) {
+        case '1':
+            calculator()
+    }
+}
+
+
+
