e05c38bb22103f75b7afa2bc97d39f7402e70a29
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2025-10-23 06:09:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-
-const mesa = [ 
-    {nome:"Mansions of madness", nota: 10},
-    {nome:"Massive darkness 2", nota: 7},
-    {nome:"Lhama", nota: 10},
-    {nome:"Destinos", nota: 6},
-]
-
-const mesanote = mesa.reduce((prev, curr) => {
-    return prev + curr.nota
-}, 0)
-
-let c = mesa.every(mesas => mesas.nota === 10)
-
-
-console.log(c)
+const promise = new Promise(() => {
+    
+})
