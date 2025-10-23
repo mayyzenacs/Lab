d4ee26e272581e098fb1edb007851cced8607037
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2025-10-23 19:22:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const promise = new Promise(() => {
     
 })
+
+
+const arr = [31, 16, 45, 98, 87, 39, 49]
+const valorProcurado = 87
+
+for (i = 0; i < arr.length; i++) {
+    if (arr[i] === valorProcurado) {
+        return console.log(arr[i])
+    }
+}
+
+
+
