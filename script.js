3998844ec2bd6aa056e321a0c115324947bbc135
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2025-10-28 21:16:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
-const promise = new Promise((resolve, reject) => {
-    const numberID = 256
+const vendas = [
+  { id: 1, produto: 'Camiseta', preco: 79.90, quantidade: 2, categoria: 'Roupas' },
+  { id: 2, produto: 'Tênis', preco: 299.99, quantidade: 1, categoria: 'Calçados' },
+  { id: 3, produto: 'Monitor', preco: 999.50, quantidade: 1, categoria: 'Eletrônicos' },
+  { id: 4, produto: 'Fone de Ouvido', preco: 149.90, quantidade: 3, categoria: 'Eletrônicos' },
+  { id: 5, produto: 'Calça Jeans', preco: 129.90, quantidade: 1, categoria: 'Roupas' },
+  { id: 6, produto: 'Teclado', preco: 250.00, quantidade: 2, categoria: 'Eletrônicos' }
+];
 
-    if (numberID === 256) {
-        resolve('Correto')
-    } else {
-        reject('Incorreto')
-    }
-})
-
-promise.then((data) => {
-    console.log(data)
-})
-
-
-const arr = [31, 16, 45, 98, 87, 39, 49]
-const valorProcurado = 87
-
-for (i = 0; i < arr.length; i++) {
-    if (arr[i] === valorProcurado) {
-        return console.log(arr[i])
-    }
-}
-
-
-
+const reduzir = vendas.reduce()
