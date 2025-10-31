0e7f1e2f37700134eff9d7fe93568368943a800d
Mayra <143430528+mayyzenacs@users.noreply.github.com>
2025-10-30 21:55:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-const vendas = [
-  { id: 1, produto: 'Camiseta', preco: 79.90, quantidade: 2, categoria: 'Roupas' },
-  { id: 2, produto: 'Tênis', preco: 299.99, quantidade: 1, categoria: 'Calçados' },
-  { id: 3, produto: 'Monitor', preco: 999.50, quantidade: 1, categoria: 'Eletrônicos' },
-  { id: 4, produto: 'Fone de Ouvido', preco: 149.90, quantidade: 3, categoria: 'Eletrônicos' },
-  { id: 5, produto: 'Calça Jeans', preco: 129.90, quantidade: 1, categoria: 'Roupas' },
-  { id: 6, produto: 'Teclado', preco: 250.00, quantidade: 2, categoria: 'Eletrônicos' }
-];
+let bin = 0
 
-const reduzir = vendas.reduce()
+
+function pro() {
+  return new Promise((resolve, reject) => {
+  reject(1)
+})
+}
+
+const result = pro().then(() => {
+  console.log('final')
+})
+.catch(() => {
+  console.log("errp")
+})
+
